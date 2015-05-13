59ea80c07a9d680a51dc0ccdfc44208cc29ac26c
Mehdi Sadri <msadri@ics.uci.edu>
2015-05-13 15:44:41 -0700

    updates the users

--- dwApp/userListGen.py
+++ dwApp/userListGen.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from mongoengine.queryset.visitor import Q
 from dwApp.models import User
 
 __author__ = 'mehdi'
@@ -12,33 +14,30 @@ def generate_users(size):
 
 
 def generate_influential_users_academia(size):
-    university_professor = User.objects.filter(userInfo__description__icontains='university').filter(
-        userInfo__description__icontains='professor').filter(userInfo__description__icontains='data')
+    prof = Q(userInfo__description__icontains='university') & Q(userInfo__description__icontains='professor')
+    school = Q(userInfo__description__icontains='university') & Q(userInfo__description__icontains='school') & Q(
+        userInfo__description__icontains='data')
+    dept = Q(userInfo__description__icontains='university') & Q(userInfo__description__icontains='department') & Q(
+        userInfo__description__icontains='data')
+    udb = Q(userInfo__description__icontains='university') & Q(userInfo__description__icontains='database')
+    edu = Q(userInfo__url__icontains='.edu') & Q(userInfo__description__icontains='data')
+    phd = Q(userInfo__description__icontains='phd') & Q(userInfo__description__icontains='student') & Q(
+        userInfo__description__icontains='data')
 
-    university_school = User.objects.filter(userInfo__description__icontains='university').filter(
-        userInfo__description__icontains='school').filter(userInfo__description__icontains='data')
+    academics = User.objects.filter(prof | school | dept | udb | edu | phd).order_by(
+        '-statistics__relevantTweetCount__value')[:size]
 
-    university_department = User.objects.filter(userInfo__description__icontains='university').filter(
-        userInfo__description__icontains='department').filter(userInfo__description__icontains='data')
-
-    university_database = User.objects.filter(userInfo__description__icontains='university').filter(
-        userInfo__description__icontains='database').filter(userInfo__description__icontains='data')
-
-    edu_website = User.objects.filter(userInfo__url__icontains='.edu').filter(userInfo__description__icontains='data')
-
-    phd_student = User.objects.filter(userInfo__description__icontains='phd').filter(
-        userInfo__description__icontains='student').filter(userInfo__description__icontains='data')
-
-    academia_users = university_professor
-    academia_users = academia_users.order_by('-statistics__relevantTweetCount__value')[:size]
-    return academia_users
+    return academics
 
 
 def generate_influential_users_industry(size):
-    industry_users = User.objects.filter(userInfo__description__icontains='works at')
-    # industry_users = industry_users | User.objects.filter(userInfo__description__icontains='entrepreneur')
-    # industry_users = industry_users | User.objects.filter(userInfo__description__icontains=' ceo ')
-    # industry_users = industry_users | User.objects.filter(userInfo__description__icontains=' cto ')
-    # industry_users = industry_users | User.objects.filter(userInfo__description__icontains='founder')
-    industry_users = industry_users.order_by('-statistics__relevantTweetCount__value')[:size]
-    return industry_users
+    works = Q(userInfo__description__icontains='works at') & Q(userInfo__description__icontains='data')
+    ent = Q(userInfo__description__icontains='entrepreneur') & Q(userInfo__description__icontains='data')
+    ceo = Q(userInfo__description__icontains=' ceo ') & Q(userInfo__description__icontains='data')
+    cto = Q(userInfo__description__icontains=' cto ') & Q(userInfo__description__icontains='data')
+    founder = Q(userInfo__description__icontains='founder') & Q(userInfo__description__icontains='data')
+
+    ind = User.objects.filter(works | ent | ceo | cto | founder).order_by(
+        '-statistics__relevantTweetCount__value')[:size]
+
+    return ind
